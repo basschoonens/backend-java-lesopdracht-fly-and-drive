0de52f4133b9522abb3ac39b27821e85cb0b7467
basschoonens <bas.schoonens@gmail.com>
2023-12-12 20:47:51 +0100

    workshop les 5 start

---
+++ Vehicle.java
@@ -0,0 +1,14 @@
+abstract public class Vehicle {
+
+    int speed;
+    float weight;
+
+    public void startEngine(){
+        System.out.println("Engine starts. Vroem vroem");
+    }
+
+    public void turnOffEngine(){
+        System.out.println("Engine turns off. Silence");
+    }
+
+}
